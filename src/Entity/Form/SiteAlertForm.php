9ce521550e9e9bd56f1eeb7af25737a89ce9456e
pfrenssen <pfrenssen@382067.no-reply.drupal.org>
2020-02-14 10:57:39 +0200

    Issue #3111360 by pfrenssen: Useless method SiteAlertForm::buildForm()

--- src/Entity/Form/SiteAlertForm.php
+++ src/Entity/Form/SiteAlertForm.php
@@ -13,16 +13,6 @@ class SiteAlertForm extends ContentEntityForm {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    $form = parent::buildForm($form, $form_state);
-    $entity = $this->entity;
-
-    return $form;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
   public function save(array $form, FormStateInterface $form_state) {
     $entity = $this->entity;
     $status = $entity->save();
